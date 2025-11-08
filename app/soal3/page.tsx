cfde687f4d10e734e3470568c2c927ef7cb8319f
rayahmd <rayasyarif@gmail.com>
2025-11-08 19:53:53 +0700

    first commit

---
+++ app/soal3/page.tsx
@@ -0,0 +1,17 @@
+export default function Soal3() {
+    return (
+        <div className="min-h-screen relative flex flex-col items-center bg-blue-600 justify-center overflow-hidden">
+            {/* matahari */}
+            <div className="bg-[#FFD000] absolute top-0 right-2 w-15 h-15 sm:w-60 sm:h-60 rounded-full z-10"></div>
+
+            <div className="w-[325px] h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-0 z-0"></div>
+            <div className="w-[325px] h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-60 z-0"></div>
+            <div className="w-[325px] h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-120 z-0"></div>
+            <div className="w-[325px] h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-180 z-0"></div>
+            <div className="w-80 h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-240 z-0"></div>
+            <div className="w-80 h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-300 z-0"></div>
+            <div className="w-80 h-[80vh] bg-white rounded-t-full rotate-180 absolute top-0 left-360 z-0"></div> 
+            <div className="w-120 h-10 bg-[#7D4C08] absolute bottom-35 justify-center"></div>
+        </div>
+    )
+}
